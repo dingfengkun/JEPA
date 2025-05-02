a4a1bd05d4394d4b565929cb60a18029068da083
Zeyuan Ding <75926621+dingfengkun@users.noreply.github.com>
2025-05-02 11:56:15 -0400

    Update main.py
    update the path for test

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def get_device():
 
 
 def load_data(device):
-    # data_path = "/scratch/DL25SP"
-    data_path = "./scratch/DL25SP"
+    data_path = "/scratch/DL25SP"
+    # data_path = "./scratch/DL25SP"
     probe_train_ds = create_wall_dataloader(
         data_path=f"{data_path}/probe_normal/train",
         probing=True,
